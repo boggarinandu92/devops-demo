2bd0b7dc00b82e82d8c658193e9a0f330b70e4ca
Nandhini M <boggari.nandu92@gmail.com>
2024-02-05 22:25:02 +0530

    added invalid username and valid password test case

--- src/test/java/test/LoginValidation.java
+++ src/test/java/test/LoginValidation.java
@@ -5,9 +5,20 @@ import org.testng.annotations.Test;
 import pom.Login;
 
 public class LoginValidation extends BaseTest{
+	
 	@Test
 	public void ValidUsernameValidPasswordTest() {
 		String username = "Sam@gmail.com";
+		String password = "Test1234";
+		Login login = new Login(driver);
+		login.loginWithCredentials(username , password);
+	}
+	/*
+	 * updated the code base from b1 branch
+	 */
+	@Test
+	public void InValidUsernameValidPasswordTest() {
+		String username = "InValid-Sam@gmail.com";
 		String password = "Test1234";
 		Login login = new Login(driver);
 		login.loginWithCredentials(username , password);
